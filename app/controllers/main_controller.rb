7bbb8a93668e63426fc1d819d3efa056abeaa15d
Elvis Rari <elvorashs759@gmail.com>
2023-07-06 11:30:10 +0300

    created a todo simple app

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+
+  def hello
+    redirect_to(action: 'index')
+  end
+  
+end
---
+++ app/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ApplicationRecord
+
+    belongs_to :category, optional: true
+end
